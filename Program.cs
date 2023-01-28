568c786b7cbee522e8adb10bcfc545537350f045
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 00:27:16 +0530

    Added: UC1 Check Employee Present/Absent

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Random rnd = new Random();
+            int isEmpPresent = rnd.Next(0, 2);
+
+            if (isEmpPresent == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
