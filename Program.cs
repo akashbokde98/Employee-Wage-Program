cb9b6200f4d78687d032024a3ad44c50af92f669
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 02:42:40 +0530

    Added: Calculate Wage For a Month

--- Program.cs
+++ Program.cs
@@ -14,23 +14,31 @@ namespace EmployeeWage
             int wagePerHr = 20;
             int fullDayHr = 8;
             int halfDayHr = 4;
+            int monthDays = 20;
+            int dailyEmpWage = 0;
 
-            switch (isEmpPresent)
+            for (int day = 1; day <= monthDays; day++) 
             {
-                case 1:
-                    int dailyEmpWage = fullDayHr * wagePerHr;
-                    Console.WriteLine("Employee is Present and Wage is:" + dailyEmpWage);
-                    break;
-            
-                case 2:
-                    int partTimeEmpWage = halfDayHr * wagePerHr;
-                    Console.WriteLine("Employee is Present half Day and Wage is:" + partTimeEmpWage);
-                    break;
+                switch ( rnd.Next(0, 3) )
+                {
+                    case 1:
+                        int dailyWage = fullDayHr * wagePerHr;
+                        dailyEmpWage += dailyWage;
+                        Console.WriteLine("Employee is Present and Wage is:" + dailyWage);
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                    case 2:
+                        int partTimeWage = halfDayHr * wagePerHr;
+                        dailyEmpWage += partTimeWage;
+                        Console.WriteLine("Employee is Present half Day and Wage is:" + partTimeWage);
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
             }
+            Console.WriteLine("Wages for a Month is:" + dailyEmpWage);
         }
     }
 }
