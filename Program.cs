c33b631697d0e3e70062ac8a5fed1b1ceedf2d16
akashbokde98 <akashbokde98@gmail.com>
2023-01-29 01:26:51 +0530

    Added: UC3 Calculate Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -9,15 +9,21 @@ namespace EmployeeWage
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
             Random rnd = new Random();
-            int isEmpPresent = rnd.Next(0, 2);
+            int isEmpPresent = rnd.Next(0, 3);
 
             int wagePerHr = 20;
             int fullDayHr = 8;
+            int halfDayHr = 4;
 
             if (isEmpPresent == 1)
             {
                 int dailyEmpWage = fullDayHr * wagePerHr;
                 Console.WriteLine("Employee is Present and Wage is:" + dailyEmpWage);
+            }
+            else if (isEmpPresent == 2) 
+            {
+                int dailyEmpWage = halfDayHr * wagePerHr;
+                Console.WriteLine("Employee is Present half Day and Wage is:" + dailyEmpWage);
             }
             else
             {
